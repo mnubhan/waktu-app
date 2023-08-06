5faa3c4b5f74ce8680ef36eb234082b3b5390ef2
mnubhan <nubhant@gmail.com>
2023-08-06 01:32:33 +0100

    fix:the api from http to https

--- src/fetchTime.js
+++ src/fetchTime.js
@@ -2,7 +2,7 @@ import { displayOthersTimezone } from "./displayOthersTimezone.js";
 import {searchCity} from "./searchCity.js";
 
 export function fetchTime(city){
-  fetch(`http://worldtimeapi.org/api/timezone/${searchCity(city)}`)
+  fetch(`https://worldtimeapi.org/api/timezone/${searchCity(city)}`)
     .then(response => response.json())
     .then(data => displayOthersTimezone(data));
 };
--- src/fetchTimeByIp.js
+++ src/fetchTimeByIp.js
@@ -1,5 +1,5 @@
 import { displayMainTimezone } from "./displayMainTimezone.js";
 
 export function fetchTimeByIp(ip){
-    fetch(`http://worldtimeapi.org/api/ip/${ip}`).then(response => response.json()).then(data => displayMainTimezone(data));
+    fetch(`https://worldtimeapi.org/api/ip/${ip}`).then(response => response.json()).then(data => displayMainTimezone(data));
   }
